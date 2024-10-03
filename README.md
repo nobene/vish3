71e8765a3bac0fca15cc87668819e5418ff3e5e3
Nobene <nobene-app@proton.me>
2024-10-03 13:54:44 +0400

    edited README.md
    OK

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Vish3
 A simple shell written in V. This barely works.
-It's a shameless fork of https://github.com/CRKatri/vsh
+It's a shameless fork of :
+ https://github.com/CRKatri/vsh
  with some improvements.
 ## Running
 `v run .`
@@ -9,7 +10,18 @@ It's a shameless fork of https://github.com/CRKatri/vsh
 `v .`
 
 ## Features :
+<<<<<<< HEAD
  It has built-in commands : help, cd , pwd, exit
  It has working prompt following scheme : user@machine:$/home/user/ >>
+=======
+ It has built-in commands : help, cd , pwd, exit.
+
+
+
+ It has working prompt following scheme :
+  `user@machine:$/home/user/ >>`
+
+
+>>>>>>> 620ba30 (edited README.md)
  It has no history, nor it is planned to have such feature.
 
--- vish3.v
+++ vish3.v
@@ -71,12 +71,31 @@ fn vsh_exec(args []string) {
 		}
 		'help' {
 			println('Vish3 Buitins: ')
-			println('  cd\n  exit\n  help\n')
+			println('  cd\n  exit\n  help\n pwd\n')
 			println('https://github.com/nobene/vish3')
 		}
+//		'|' {
+//			println(args[0])
+//		}
+//		"[']" {
+//			exit(0)
+//		}
 		else {
-//			println(args)
+			if args[0].len == 0 {
+//				println('vish3 error: zero input')
+			return
+			}
+//			println(args[0].bytes())
+			if args[0].bytes()[0] == 27 && args[0].len == 1 {
+			println('vish3 stopped by pressed ESC...')
+				exit(0)
+			}
 //			println('\n')
+			if '|' in args {
+			vsh_exec_piped(args)
+			println('returning from vsh_exec')
+			return
+			}
 			if re1.matches_string(args[0].split(' >> ')[0]) {
 //				println('dbg: matched . or /')
 				vsh_launch(args)
@@ -92,6 +111,107 @@ fn vsh_exec(args []string) {
 	}
 }
 
+fn vsh_exec_piped(args []string) {
+	if args.len == 0 {
+		return
+	}
+//	query1 := r'^[\.\/]'
+//	query2 := r'^\W{2,}'
+//	mut re1 := regex.regex_opt(query1) or { panic(err) }
+//	mut re2 := regex.regex_opt(query2) or { panic(err) }
+	match args[0] {
+//		'exit' {
+//			if args.len > 1 {
+//				exit(args[1].int())
+//			} else {
+//				exit(0)
+//			}
+//		}
+		'cd' {
+			mut path := home_dir()
+			if args.len > 1 {
+				path = expand_tilde_to_home(args[1])
+			}
+			chdir(path) or {
+				eprintln(err)
+				return
+			}
+		} else {
+			if args[0].len == 0 {
+//				println('vish3 error: zero input')
+			return
+			}
+//			println(args[0].bytes())
+			if args[0].bytes()[0] == 27 && args[0].len == 1 {
+			println('vish3 stopped by pressed ESC...')
+				exit(0)
+			}
+			if args[0].bytes()[0] == 124 {
+				println('error: "|" can not be 1st char of command')
+				return
+			}
+			println('in _piped \n')
+//			println(args)
+//			println('\n')
+			println(args[0])
+			if u8(124) in args[0].bytes() {
+				println('error: no spaces around "|" in argument 1')
+				return
+			}
+			if args.len > 2 {
+				if u8(124) in args[2].bytes() {
+					println('error: no spaces around "|" in argument 3')
+					return
+				}
+			}
+			if args.len > 1 {
+				println(args[1])
+			}
+			if args.len > 2 {
+				println(args[2])
+				println(args[3])
+				println('\n')
+				mut cmd0 := os.find_abs_path_of_executable(args[0]) or { args[0] }
+				if cmd0.len == 0 {
+					cmd0 = 'test'
+				}
+				mut p0 := os.new_process(cmd0)
+				p0.run()
+				p0.set_redirect_stdio()
+//				r := p0.pipe_read(.stdout).str()
+//				if p0.is_pending(.stdout) { dump( p0.stdout_read() ) }
+				r := p0.stdout_read()
+				p0.close()
+				mut cmd2 := os.find_abs_path_of_executable(args[0]) or { args[0] }
+				if cmd2.len == 0 {
+					cmd2 = 'test'
+				}
+				mut p2 := os.new_process(cmd2)
+//				ln := args.len - 1
+				p2.set_args(args[3..3])
+				p2.run()
+				p2.set_redirect_stdio()
+				p2.stdin_write(r)
+				p0.wait()
+				p2.wait()
+//				println(r)
+				return
+			}
+//			if re1.matches_string(args[0].split(' >> ')[0]) {
+//				println('dbg: matched . or /')
+//				vsh_launch(args)
+//				return
+//			}
+//			if re2.matches_string(args[0].split(' >> ')[0]) {
+//				println('dbg: matched stop char')
+//				return
+//				exit(0)
+//			}
+//			vsh_launch(args)
+		}
+	}
+}
+
 fn vsh_launch(args []string) {
 	mut cmd := 'test'
 //	println(cmd)
@@ -101,7 +221,7 @@ fn vsh_launch(args []string) {
 //	println('mark 2')
 //	println( args[1])
 	if args[0].len == 0 {
-		println('vsh: zero input')
+		println('vish3 : zero input')
 		return
 	}
 	if args[0][0] == byte(`/`) {
@@ -120,5 +240,9 @@ fn vsh_launch(args []string) {
 //	}
 	p.run()
 	p.wait()
+	return
 }
+
+
+
 
